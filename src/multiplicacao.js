aed079fad65f0cd80b572655d3bee5a3664745ca
S4ntos25 <132910423+S4ntos25@users.noreply.github.com>
2025-06-14 17:40:52 -0300

    Update multiplicacao.js

---
+++ src/multiplicacao.js
@@ -0,0 +1,10 @@
+"use strict";
+function multiplicar(a, b) {
+    return a * b;
+}
+function saudar(nome) {
+    return "Olá " + nome;
+}
+// Exemplos de uso
+console.log(multiplicar(5, 3));
+console.log(saudar("Vitor"));

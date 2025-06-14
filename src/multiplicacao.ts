71f3e5c9b8ec0b14a35a60ac041c4dc2fa5e09b5
S4ntos25 <132910423+S4ntos25@users.noreply.github.com>
2025-06-14 17:41:30 -0300

    Update multiplicacao.ts

---
+++ src/multiplicacao.ts
@@ -0,0 +1,12 @@
+    function multiplicacacao(a: number, b: number): number {
+        return a * b;
+    }
+    
+    function valor(nome: string): string {
+        return "Olá " + nome;
+    }
+    
+    // Exemplos de uso
+    console.log(multiplicacacao(5, 3)); 
+    console.log(valor("Vitor")); 
+    
